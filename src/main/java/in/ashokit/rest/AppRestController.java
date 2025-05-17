1374cd93a0b5c73a3824d38091e553d453ffa3a4
merry <lipsa4645@gmail.com>
2025-05-17 12:38:38 +0530

    first commit

---
+++ src/main/java/in/ashokit/rest/AppRestController.java
@@ -0,0 +1,26 @@
+package in.ashokit.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppRestController {
+	
+	private Logger logger= LoggerFactory.getLogger(AppRestController.class);
+
+	@GetMapping("/greet")
+	public String greetMsg() {
+
+		logger.debug("this is debug msg from greet.....");
+		logger.info("greetMsg() execution started...");
+		String msg = "Good Morning...";
+
+		logger.warn("This is warning from greet method...");
+
+		logger.info("greetMsg() execution ended...");
+		return msg;
+	}
+
+}
